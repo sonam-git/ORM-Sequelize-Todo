f7a0976f8e26e6f0eb0c2da0c398477463889f16
Sonam Sherpa <sherpa.sjs@gmail.com>
2023-12-20 21:43:49 -0800

    css updated for handlebars template

--- server.js
+++ server.js
@@ -1,31 +1,34 @@
 const express = require("express");
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
+const helpers = require("./utils");
 const hbs = exphbs.create({
   // how you want to connect express with handlebars
+  helpers,
 });
-const session = require('express-session');
+const session = require("express-session");
 const sequelize = require("./config/connection");
+
 const app = express();
 
-const routes = require('./routes')
+const routes = require("./routes");
 
 const PORT = process.env.PORT || 3001;
 
 // (set up handlebar engine) setup express so that it knows we are using handlebars as our template engine
 // sets up the Handlebars engine with its default configurations.
-app.engine('handlebars',hbs.engine);
+app.engine("handlebars", hbs.engine);
 // tells Express to use 'handlebars' as the default file extension for views.
-app.set('view engine','handlebars');
+app.set("view engine", "handlebars");
 
 // setup express to use sessions & create cookie on the browser
 const sessionConfig = {
-  secret: 'super secret',
+  secret: "super secret",
   resave: false,
   saveUninitialized: false,
-}
+};
 
 // middlewares
-app.use(express.static(__dirname + '/public'));  // __dirname: current dir the server.js is in
+app.use(express.static(__dirname + "/public")); // __dirname: current dir the server.js is in
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session(sessionConfig)); //this will create req.session object for every req that comes into our server
@@ -34,5 +37,5 @@ app.use(routes);
 
 // sync db and start the server
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log(`server running at port 3001`));
+  app.listen(PORT, () => console.log(`server running at ${PORT}`));
 });
--- utils/index.js
+++ utils/index.js
@@ -4,5 +4,13 @@ module.exports = {
   format_date: (date) => {
     return DateTime.fromJSDate(date).toFormat('ff');
   },
-  toUpperCase: (str) => str.toUpperCase(),
+  capitalizeFirstLetter: (str) => {
+    // Ensure that the input is a string
+    if (typeof str !== 'string') {
+      throw new Error('Input must be a string');
+    }
+
+    // Capitalize the first letter and convert the rest to lowercase
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  },
 };
